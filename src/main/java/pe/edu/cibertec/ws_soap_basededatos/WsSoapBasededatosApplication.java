1b5bed38fd7940181b65959385363606ed9c12a7
sour15 <ashleeagres56@gmail.com>
2024-10-13 09:08:46 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/ws_soap_basededatos/WsSoapBasededatosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_soap_basededatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSoapBasededatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSoapBasededatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/ws_soap_basededatos/WsSoapBasededatosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_soap_basededatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSoapBasededatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
